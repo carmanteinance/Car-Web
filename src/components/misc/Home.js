746573b941773f5527cdd16858652376ada0b3da
Nerea <nereaparicio@gmail.com>
2019-04-04 22:23:45 +0200

    Components Home

--- src/components/misc/Home.js
+++ src/components/misc/Home.js
@@ -1,18 +1,20 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-export default () => {
+const Home = () => {
   return (
     <div className="row text-center mt-5">
-      <div className="col-12 mb-2">
-        <Link className="btn btn-primary" to="/cars-list"><i className="fa fa-tag"/> Mis coches</Link>
+      <div className="col-12 mb-4">
+        <Link className="btn btn-secondary" to="/cars-list"> <i className="fa fa-car mr-2"/>  MIS COCHES</Link>
       </div>
-      <div className="col-12">
-        <Link className="btn btn-primary" to="/cars-list">Mis coches</Link>
+      <div className="col-12 mb-4">
+        <Link className="btn btn-secondary" to="/alert-list"> <i className="fa fa-exclamation-triangle mr-2"/>  ALERTAS</Link>
       </div>
-      <div className="col-12">
-        <Link className="btn btn-primary" to="/cars-list">Mis coches</Link>
+      <div className="col-12 ">
+        <Link className="btn btn-secondary" to="/talleres"> <i className="fa fa-tools mr-2"/> TALLERES</Link>
       </div>
     </div>
   );
 }
+
+export default Home;
--- src/components/misc/NavBar.js
+++ src/components/misc/NavBar.js
@@ -5,7 +5,7 @@ class NavBar extends Component {
   render(){
     return(
     <nav className="navbar navbar-expand-lg navbar navbar-dark bg-dark">
-      <a className="navbar-brand" href="/profile"> Welcome</a>
+      <a className="navbar-brand text-white"> Welcome "User"</a>  
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
@@ -14,10 +14,10 @@ class NavBar extends Component {
         <li className="nav-item active">
           <a className="nav-link fa fa-user-circle" href="/profile"> Profile <span className="sr-only">(current)</span></a>
         </li>
-        <li className="nav-item ml-2">
+        <li className="nav-item active ml-2">
           <a className="nav-link fa fa-car" href="/my-cars"> My Cars</a>
         </li>
-        <li className="nav-item ml-2">
+        <li className="nav-item active ml-2">
           <a className="nav-link fa fa-sign-out-alt" href="/logout"> Logout</a>
         </li>
       </ul>
