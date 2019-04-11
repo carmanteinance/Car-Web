4474fc8e2a74ffdf2dcb5e5b35337f718e4efac5
Nerea <nereaparicio@gmail.com>
2019-04-11 22:33:36 +0200

    working on Add Car

--- src/components/maintenance/Car/CarItem.js
+++ src/components/maintenance/Car/CarItem.js
@@ -2,6 +2,9 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const CarItem = ({ brand, model, carNumber, year, id, deleteCar }) => {
+
+  // {year.getDay()}{year.getMonth()}{year.getFullYear()}
+
   return (
     <tr>
       <td>
--- src/services/CarService.js
+++ src/services/CarService.js
@@ -1,17 +1,20 @@
 import http from "./BaseService";
 
 const registerCar = car =>
-  http.post("/my-cars/newCar", car).then(response => response.data);
+  http.post("/my-cars/newCar", car)
+    .then(response => response.data);
 
 const listCar = () =>
-  http.get("/my-cars").then(res => Promise.resolve(res.data));
+  http.get("/my-cars")
+    .then(res => Promise.resolve(res.data));
 
 const listOneCar = () =>
   http.get("/my-cars/:id")
     .then(res => Promise.resolve(res.data));
 
 const addCar = car =>
-  http.post("/my-cars/newCar ", car).then(response => response.data);
+  http.post("/my-cars/newCar ", car)
+    .then(response => response.data);
 
 // const updateProfile = (user) => {
 //   const data = new FormData();
@@ -23,7 +26,8 @@ const addCar = car =>
 // }
 
 const deleteCar = id =>
-  http.delete(`/my-cars/delete/${id}`).then(response => response.data);
+  http.delete(`/my-cars/delete/${id}`)
+    .then(response => response.data);
 
 export default {
   registerCar,
